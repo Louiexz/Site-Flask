14408f628649d71ab35ad9baae97e8bbe31ebe23
Louiexz <fluizlucas@gmail.com>
2024-03-26 03:05:15 -0300

    Corrigindo

--- site/app.py
+++ site/app.py
@@ -1,25 +1,39 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+import uuid
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
-secret_key = os.urandom(24)
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secret_key.hex()
+app.config['SECRET_KEY'] = os.urandom(24)
+
 socketio = SocketIO(app)
+
+users = 0
+usuarios_conectados = []
 
 @app.route("/")
 def index(): return render_template("index.html")
 
-@app.route("/subtemplates/chat")
-def chat(): return render_template("subtemplates/chat.html")
+@app.route("/chatzin/chat")
+def chat(): return render_template("chatzin/chat.html")
 
-@app.route("/subtemplates/about")
-def sobre(): return render_template("subtemplates/about.html")
+@app.route("/chatzin/about")
+def sobre(): return render_template("chatzin/about.html")
+
+def enviar_mensagem(message, bol=True): emit('message', {'message': message}, broadcast=bol)
+
+def pegaUser():
+    for user_info in usuarios_conectados:
+        if user_info[1] == request.sid: return user_info[0]
+
+@socketio.on('connect')
+def handle_connect(): enviar_mensagem('Usuário conectado')
+
+@socketio.on('disconnect')
+def handle_disconnected(): enviar_mensagem('Usuário desconectado ' + str(user_id), False)
 
 @socketio.on('message')
-def handle_message(message):
-    print(message)
-    emit('message', {'username': 'Estranho', 'message': message}, broadcast=True)
+def handle_message(data): enviar_mensagem(data)
 
-if __name__ == "__main__": socketio.run(app)
+if __name__ == "__main__":
+    socketio.run(app)
--- site/static/js/script.js
+++ site/static/js/script.js
@@ -4,26 +4,19 @@ function conectar() {
     document.getElementById("conexao").style.display = "none";
     socket = io.connect('http://' + document.domain + ":" + location.port);
 
-    socket.on('connect', function() {
-        socket.send('Usuário conectado!');
-    });
-
     socket.on('message', function(data) {
         var p = document.createElement('p');
-        var username = (data.username === 'Eu') ? 'Eu': 'Estranho';
-        p.innerHTML = '<strong>' + username + '</strong>: ' + data["message"];
-        
-        p.classList.add((username === 'Eu') ? 'eu': 'estranho');
-        
+        p.innerHTML = '<strong>Estranho</strong>: ' + data['message'];
         document.getElementById('area-chat').appendChild(p);
     });
 }
-function sendMessage() {
+
+function enviarMensagem() {
     var message = document.getElementById('message').value;
     try {
         socket.emit('message', message);
         document.getElementById('message').value = '';
-    }catch(err){
-        console.log("Algo errado" + err);
+    } catch(err) {
+        console.log("Algo deu errado: " + err);
     }
 }

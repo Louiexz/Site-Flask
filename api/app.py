adf2c6ede1180bcc6b1303b97a3cb378c6b57ee5
Louiexz <fluizlucas@gmail.com>
2024-05-27 22:59:02 -0300

    Teste

--- api/app.py
+++ api/app.py
@@ -33,7 +33,7 @@ def handle_message(data):
     sender_sid = data.get('sender_sid')
     username_slice_start = data.get('username_slice_start')
     username_slice_end = data.get('username_slice_end')
-    username = usuarios_conectados.get(sender_sid, f'Anonymous{contador_anonymous}')[username_slice_start:username_slice_end]
+    username = usuarios_conectados.get(sender_sid, f'Anônimo{contador_anonymous}')[username_slice_start:username_slice_end]
     message = data.get('msg')
     send({'username': username, 'msg': message}, broadcast=True, include_self=True)
 
@@ -42,18 +42,15 @@ def handle_message(data):
 def handle_connect():
     global contador_anonymous
     global usuarios_conectados
-    username = f'Anonymous{contador_anonymous}'
+    username = f'Anônimo{contador_anonymous}'
     usuarios_conectados[request.sid] = username
     contador_anonymous += 1
-    print('Usuário conectado:', request.sid)
 
 # Função para manipular desconexões de cliente
 @socketio.on('disconnect')
 def handle_disconnected():
     global usuarios_conectados
-    print('Usuário desconectado:', request.sid)
-    if request.sid in usuarios_conectados:
-        del usuarios_conectados[request.sid]
+    if request.sid in usuarios_conectados: del usuarios_conectados[request.sid]
 
 # Verifica se o script está sendo executado diretamente
 if __name__ == "__main__":
--- api/static/js/script.js
+++ api/static/js/script.js
@@ -2,20 +2,21 @@ var socket;
 
 function conectar() {
     // Conectando ao servidor SocketIO
-    socket = io.connect(`https://${document.domain}:${location.port}`);
+    $('#conectar').hide();
+    try {
+        socket = io.connect(`//${document.domain}:${location.port}`);
+        // Função executada quando a conexão é estabelecida
+        socket.on('connect', function() {
+            // Envia uma mensagem indicando que o usuário se conectou
+            socket.send({'username': '', 'msg': 'Usuário conectado!', 'sender_sid': socket.id});
 
-    // Função executada quando a conexão é estabelecida
-    socket.on('connect', function() {
-        // Envia uma mensagem indicando que o usuário se conectou
-        socket.send({'username': '', 'msg': 'Usuário conectado!', 'sender_sid': socket.id});
+        });
 
-        $('#conectar').hide();
-    });
-
-    // Define a função de manipulação de eventos para mensagens recebidas
-    socket.on('message', function(data) {
-        $('#area-chat').append($('<p><strong>' + data.username + '</strong>: ' + data.msg + '</p>'));
-    });
+        // Define a função de manipulação de eventos para mensagens recebidas
+        socket.on('message', function(data) {
+            $('#area-chat').append($('<p><strong>' + data.username + '</strong>: ' + data.msg + '</p>'));
+        });
+    } catch (e) { console.log('Erro encontrado: ' + e) }
 }
 
 // Função para enviar mensagem
@@ -35,7 +36,7 @@ function enviarMensagem() {
 // Evento DOMContentLoaded para garantir que o código seja executado após o carregamento do DOM
 $(document).ready(function() {
     // Evento de click para conectar-se
-    // $('#conectar').click(conectar);
+    $('#conectar').click(conectar);
 
     // Define o evento de clique para o botão de envio de mensagem
     $('#envio').click(enviarMensagem);
